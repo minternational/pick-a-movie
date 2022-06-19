63cc0a4d8a485a5ab8f2e281648d1f572d6c5640
Christopher <54006760+minternational@users.noreply.github.com>
2022-06-19 19:26:51 +0200

    Add files via upload
    fixed background issue and responsive design

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+const elementMovieList = document.getElementById('movieList');
+const elementRecommendation = document.getElementById('recommendation');
+
 let movieArray = [];
 
 document.getElementById('insertMovieToArray').addEventListener('click', () => {
   const getInputValue = document.getElementById('input').value;
+
   if (!getInputValue) {
-    document.getElementById('movieList').style.visibility = 'visible';
-    document.getElementById('movieList').innerHTML =
-      'Please type in a movie or series!';
+    alert('Please type in a movie or series!');
   } else {
     movieArray.push(getInputValue);
     document.getElementById('input').value = '';
-    document.getElementById('movieList').innerHTML = movieArray.join(', ');
-    document.getElementById('movieList').style.visibility = 'visible';
+    elementMovieList.innerHTML = movieArray.join(', ');
+    elementMovieList.style.visibility = 'visible';
   }
 });
 
@@ -18,24 +20,24 @@ document.getElementById('showMovieInDom').addEventListener('click', () => {
   if (movieArray.length) {
     recommendation.innerHTML =
       movieArray[Math.floor(Math.random() * movieArray.length)];
-    document.getElementById('recommendation').style.visibility = 'visible';
+    elementRecommendation.style.visibility = 'visible';
   } else {
-    document.getElementById('recommendation').style.visibility = 'visible';
+    elementRecommendation.style.visibility = 'visible';
     recommendation.innerHTML = 'You need to type in a movie or series!';
   }
 });
 
 document.getElementById('deleteLastItem').addEventListener('click', () => {
   movieArray.pop();
-  document.getElementById('movieList').innerHTML = movieArray.join(', ');
+  elementMovieList.innerHTML = movieArray.join(', ');
   recommendation.innerHTML = '';
-  document.getElementById('recommendation').style.visibility = 'hidden';
+  elementRecommendation.style.visibility = 'hidden';
 });
 
 document.getElementById('deleteAllItems').addEventListener('click', () => {
   movieArray = [];
-  document.getElementById('movieList').innerHTML = '';
+  elementMovieList.innerHTML = '';
   recommendation.innerHTML = '';
-  document.getElementById('recommendation').style.visibility = 'hidden';
-  document.getElementById('movieList').style.visibility = 'hidden';
+  elementRecommendation.style.visibility = 'hidden';
+  elementMovieList.style.visibility = 'hidden';
 });
